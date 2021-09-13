38a4bbc51fb5ab47b01b9b4b7419cfbde9aa16b2
Ahmad Jalaludin <90632263+25Jalaludin@users.noreply.github.com>
2021-09-13 08:11:31 -0700

    Add files via upload

---
+++ #Tugas latihan 1.py
@@ -0,0 +1,6 @@
+#Tugas latihan 1
+print("========================================")
+print("Nama : Ahmad Jalaludin")
+print("Kelas : 17.1A.25")
+print("Jurusan : Teknologi Informasi")
+print("========================================")
